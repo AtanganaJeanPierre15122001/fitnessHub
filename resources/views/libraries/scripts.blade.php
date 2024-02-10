20779cfdfae503a6339712841fd0f0ac5ba52362
AtanganaJeanPierre15122001 <115159198+AtanganaJeanPierre15122001@users.noreply.github.com>
2024-02-10 23:22:58 +0100

    Initial commit

---
+++ resources/views/libraries/scripts.blade.php
@@ -0,0 +1,12 @@
+
+
+<script src="{{asset('assets/js/jquery-3.3.1.min.js')}}"></script>
+<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
+<script src="{{asset('assets/js/mixitup.min.js')}}"></script>
+<script src="{{asset('assets/js/jquery.slicknav.js')}}"></script>
+<script src="{{asset('assets/js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('assets/js/main.js')}}"></script>
+
+
+@stack('js')
---
+++ resources/views/libraries/styles.blade.php
@@ -0,0 +1,14 @@
+<!-- General CSS Files -->
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900&display=swap"
+      rel="stylesheet">
+
+<!-- Css Styles -->
+<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}" type="text/css">
+<link rel="stylesheet" href="{{asset('assets/css/font-awesome.min.css')}}" type="text/css">
+<link rel="stylesheet" href="{{asset('assets/css/owl.carousel.min.css')}}" type="text/css">
+<link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}" type="text/css">
+<link rel="stylesheet" href="{{asset('assets/css/slicknav.min.css')}}" type="text/css">
+<link rel="stylesheet" href="{{asset('assets/css/style.css')}}" type="text/css">
+
+
+@stack('css')
