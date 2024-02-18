5b86dd0706bd89dd1595df713fcb89bd2e13f97d
kevinemalone <nyamitchoumbamalone@gmail.com>
2024-02-18 07:19:32 +0100

    modification des interface

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('welcome');
 });
+
+Route::get('/', function () {
+    return view('blog/index');
+});
+
+
 
 
